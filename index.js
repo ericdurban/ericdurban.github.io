70ab54025fb632d871c4a61b79683187109d8dbf
Eric Urban <ericdurban32@gmail.com>
2025-03-27 09:15:43 -0700

    Added my course certificate to show I've completed my bootcamp!

--- index.js
+++ index.js
@@ -66,6 +66,36 @@ rotateText();
 setInterval(rotateText, 4000); // Rotate every 4 seconds.
 
 
+////////////////////////////////
+//Course Certificate PDF Modal//
+////////////////////////////////
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the element that opens the modal
+var btn = document.getElementById("modalTrigger");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
 //////////////////////////
 //Project Drop Down List//
 //////////////////////////
